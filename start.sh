5a9729a4af543149ba90f7145da2440c942de007
Han <allen0346@gmail.com>
2020-05-18 16:27:59 +0800

    Adjust: update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ awsPath=/usr/bin/aws
 cd /home/ec2-user/code/google-sheet-uploader
 $nodePath /home/ec2-user/code/google-sheet-uploader/src/index.js &&
 $nodePath /home/ec2-user/code/google-sheet-uploader/src/utils/uploader.js &&
-$awsPath cloudfront create-invalidation --distribution-id E2RE5FZJI8MX89 --paths "/recall-vote-han-kuo-yu/editor.json"
+$awsPath cloudfront create-invalidation --distribution-id E2RE5FZJI8MX89 --paths "/recall-vote-han-kuo-yu/content.json" "/recall-vote-han-kuo-yu/takeaway.json"
